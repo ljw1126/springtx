b3c6c1a5c026c4d55f264b010d992a69b501d452
leejinwoo <leejinwoo1126@gmail.com>
2023-12-21 20:02:34 +0900

    feat: 섹션11 예제 프로젝트 코드

---
+++ src/main/java/hello/springtx/propagation/MemberRepository.java
@@ -0,0 +1,27 @@
+package hello.springtx.propagation;
+
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Repository
+@RequiredArgsConstructor
+public class MemberRepository {
+
+    private final EntityManager em;
+
+    @Transactional
+    public void save(Member member) {
+        em.persist(member);
+    }
+
+    @Transactional(readOnly = true)
+    public Optional<Member> find(String username) {
+        return em.createQuery("select m from Member m where m.username = :username ", Member.class)
+                .setParameter("username", username)
+                .getResultList().stream().findFirst();
+    }
+}

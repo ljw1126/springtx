90020aff0ccd04628bbdc0dd68234ba81daa4585
leejinwoo <leejinwoo1126@gmail.com>
2023-12-22 19:34:15 +0900

    test: 예외 커밋, 롤백 테스트 추가

--- src/test/java/hello/springtx/propagation/MemberServiceTest.java
+++ src/test/java/hello/springtx/propagation/MemberServiceTest.java
@@ -2,9 +2,13 @@ package hello.springtx.propagation;
 
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.UnexpectedRollbackException;
+
+import java.rmi.UnexpectedException;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -54,6 +58,103 @@ class MemberServiceTest {
                 .isInstanceOf(RuntimeException.class);
 
         //when
+        assertThat(memberRepository.find(username).isPresent()).isTrue();
+        assertThat(logRepository.find(username).isEmpty()).isTrue();
+    }
+
+
+    /**
+     * MemberService        @Transactional: ON
+     * MemberRepository     @Transactional: OFF
+     * LogRepository        @Transactional: OFF
+     */
+    @Test
+    void singleTx() {
+        //given
+        String username = "singleTx";
+
+        //when
+        memberService.joinV1(username);
+
+        //when
+        assertThat(memberRepository.find(username).isPresent()).isTrue();
+        assertThat(logRepository.find(username).isPresent()).isTrue();
+    }
+
+    /**
+     * MemberService        @Transactional: ON
+     * MemberRepository     @Transactional: ON
+     * LogRepository        @Transactional: ON
+     */
+    @Test
+    void outerTxOn_success() {
+        //given
+        String username = "outerTxOn_success";
+
+        //when
+        memberService.joinV1(username);
+
+        //when
+        assertThat(memberRepository.find(username).isPresent()).isTrue();
+        assertThat(logRepository.find(username).isPresent()).isTrue();
+    }
+
+    /**
+     * MemberService        @Transactional: ON
+     * MemberRepository     @Transactional: ON
+     * LogRepository        @Transactional: ON throw RuntimeException
+     */
+    @Test
+    void outerTxOn_fail() {
+        //given
+        String username = "로그예외_outerTxOff_fail";
+
+        //when
+        assertThatThrownBy(() -> memberService.joinV1(username))
+                .isInstanceOf(RuntimeException.class);
+
+        //when
+        assertThat(memberRepository.find(username).isEmpty()).isTrue();
+        assertThat(logRepository.find(username).isEmpty()).isTrue();
+    }
+
+
+    /**
+     * MemberService        @Transactional: ON
+     * MemberRepository     @Transactional: ON
+     * LogRepository        @Transactional: ON throw RuntimeException
+     */
+    @DisplayName("실무에서도 실수할 수 있는 중요 부분**")
+    @Test
+    void recoverException_fail() {
+        //given
+        String username = "로그예외_recoverException_fail";
+
+        //when
+        assertThatThrownBy(() -> memberService.joinV2(username))
+                .isInstanceOf(UnexpectedRollbackException.class);
+
+        //when 둘다 롤백
+        assertThat(memberRepository.find(username).isEmpty()).isTrue();
+        assertThat(logRepository.find(username).isEmpty()).isTrue();
+    }
+
+
+    /**
+     * MemberService        @Transactional: ON
+     * MemberRepository     @Transactional: ON
+     * LogRepository        @Transactional(propagation = Propagation.REQUIRES_NEW): ON throw RuntimeException
+     */
+    @DisplayName("전파옵션을 설정해서 로그예외 발생해도 멤버는 정상처리된다")
+    @Test
+    void recoverException_success() {
+        //given
+        String username = "로그예외_recoverException_success";
+
+        //when
+        memberService.joinV2(username);
+
+        //when 맴버 저장, 로그 롤백
         assertThat(memberRepository.find(username).isPresent()).isTrue();
         assertThat(logRepository.find(username).isEmpty()).isTrue();
     }
